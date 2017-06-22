1ca979999b12482925cf46ef881ab90f16e22cbc
mauricio.togneri <mauricio.togneri@gmail.com>
2017-06-22 23:17:23 +0200

    Improved TextLink

--- androidutils/src/main/java/com/mauriciotogneri/androidutils/TextLink.java
+++ androidutils/src/main/java/com/mauriciotogneri/androidutils/TextLink.java
@@ -14,14 +14,58 @@ import java.util.regex.Pattern;
 
 public class TextLink
 {
+    private final TextView target;
+    private final String text;
     private final String pattern;
+    private final Integer color;
+    private final Boolean underline;
+    private final Boolean bold;
+    private final Integer size;
+    private final LinkClickCallback callback;
 
-    public TextLink(String pattern)
+    private TextLink(TextView target, String text, String pattern, Integer color, Boolean underline, Boolean bold, Integer size, LinkClickCallback callback)
     {
+        this.target = target;
+        this.text = text;
         this.pattern = pattern;
+        this.color = color;
+        this.underline = underline;
+        this.bold = bold;
+        this.size = size;
+        this.callback = callback;
     }
 
-    public void format(TextView textView, String text, final @ColorInt int color, final boolean underline, final LinkClickListener listener)
+    private TextLink(TextView target, String text, String pattern)
+    {
+        this(target, text, pattern, null, null, null, null, null);
+    }
+
+    public TextLink color(@ColorInt int color)
+    {
+        return new TextLink(target, text, pattern, color, underline, bold, size, callback);
+    }
+
+    public TextLink underline(Boolean underline)
+    {
+        return new TextLink(target, text, pattern, color, underline, bold, size, callback);
+    }
+
+    public TextLink bold(Boolean bold)
+    {
+        return new TextLink(target, text, pattern, color, underline, bold, size, callback);
+    }
+
+    public TextLink size(Integer size)
+    {
+        return new TextLink(target, text, pattern, color, underline, bold, size, callback);
+    }
+
+    public TextLink callback(LinkClickCallback callback)
+    {
+        return new TextLink(target, text, pattern, color, underline, bold, size, callback);
+    }
+
+    public void format()
     {
         Matcher matcher = Pattern.compile(pattern).matcher(text);
 
@@ -39,26 +83,46 @@ public class TextLink
                 @Override
                 public void onClick(View textView)
                 {
-                    listener.onClick(link);
+                    if (callback != null)
+                    {
+                        callback.onClick(link);
+                    }
                 }
 
                 @Override
                 public void updateDrawState(TextPaint textPaint)
                 {
-                    textPaint.setUnderlineText(underline);
-                    textPaint.setColor(color);
+                    if (color != null)
+                    {
+                        textPaint.setColor(color);
+                    }
+
+                    if (underline != null)
+                    {
+                        textPaint.setUnderlineText(underline);
+                    }
+
+                    if (bold != null)
+                    {
+                        textPaint.setFakeBoldText(bold);
+                    }
+
+                    if (size != null)
+                    {
+                        textPaint.setTextSize(size);
+                    }
                 }
             };
 
             SpannableString spannable = new SpannableString(finalText);
             spannable.setSpan(clickableSpan, startIndex, startIndex + link.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
-            textView.setMovementMethod(LinkMovementMethod.getInstance());
-            textView.setText(spannable);
+            target.setMovementMethod(LinkMovementMethod.getInstance());
+            target.setText(spannable);
         }
     }
 
-    public interface LinkClickListener
+    public interface LinkClickCallback
     {
         void onClick(String text);
     }
