fe4fa71d48b89a073214f025e378b02580be556f
Mauricio Togneri <mauricio.togneri@gmail.com>
2018-09-08 01:21:09 +0200

    Added compat file provider

---
+++ androidutils/src/main/java/com/mauriciotogneri/androidutils/CompatFileProvider.java
@@ -0,0 +1,17 @@
+package com.mauriciotogneri.androidutils;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
+
+import com.commonsware.cwac.provider.LegacyCompatCursorWrapper;
+
+public class CompatFileProvider extends FileProvider
+{
+    @Override
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder)
+    {
+        return (new LegacyCompatCursorWrapper(super.query(uri, projection, selection, selectionArgs, sortOrder)));
+    }
+}
